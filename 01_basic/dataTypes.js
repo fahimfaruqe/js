353a8c7e93840e476bf193b5312f81b28c39f8d4
fahimfaruqe <fahim.faruqe7720@gmail.com>
2023-12-30 01:16:54 +0600

    dataTypes

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+//alert(7+7) it runs only browser not node js
+
+let name="fahim"
+let age=20
+let isLoggedIn=false
+let state=null
+
+//number=2to the power 53
+//bigint=boro boro num er jonno(stock market,cripto eisob e)
+//string="" er vitor rskhte hoy
+//boolean=true or false
+//null = standalone value kono value e nai
+//undefined=variable declare korsi but value add kori nai
+//symbol=uniqe kisu use korle
+//object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
---
+++ 01_basic/variables.js
@@ -0,0 +1,10 @@
+const accountId = 21335797 // const mani ja dibe oitai lock
+let accountEmail = "fahim123@gmail.com"
+var accountPassword = "fahimfahim"
+accountCity = "Cumilla"
+let accountState; //shudhu variable declear korle ans=undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
