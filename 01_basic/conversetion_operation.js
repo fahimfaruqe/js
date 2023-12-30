a105219d9df05a815e2d185ed58307da901ade10
fahimfaruqe <fahim.faruqe7720@gmail.com>
2023-12-30 13:47:17 +0600

    dataType_Summery

---
+++ 01_basic/conversetion_operation.js
@@ -0,0 +1,51 @@
+let score=true
+//console.log(typeof score);
+let valueTotal= Number(score)
+//console.log(typeof valueTotal);
+//console.log(valueTotal);
+//33=convert 33
+//"33asd"=NaN
+//true=1 & false=0
+
+let isLoggingIn =0
+//console.log(typeof isLoggingIn);
+let isLog= Boolean(isLoggingIn)
+//console.log(typeof isLog);
+//console.log(isLog);
+//1=true & 0=false
+//""=false
+//"value"=true
+
+let someNum = 22
+//console.log(typeof someNum);
+let convNum = String(someNum)
+//console.log(typeof convNum);
+//console.log(convNum);
+
+
+
+//***********Operations****** */
+let value= 3
+let negValue= -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/3);
+//console.log(2%3);
+//console.log(2**3); **=2 er upor power
+
+//console.log(1+"2");
+//console.log("1" + 2);
+//console.log("1"+2+3);
+//console.log("1"+"2"+3);
+//console.log(1+2+"3");
+//console.log((1+2)*5%3);
+//console.log([{(1+2)*5}%3]);
+
+
+let num = 3
+//num++
+++num
+console.log(num);
---
+++ 01_basic/dataType-summery.js
@@ -0,0 +1,16 @@
+ /*datatype have 2 parts
+ 1.preemetive
+ 2.non-preemetive
+ 
+ 
+ primitive=7 catagories
+ string,number,null,boolean,undefined,symbol,bigInt
+ 
+ 
+ Reference type(non-preemetive) 3 types
+ array,object,function*/
+ const heros =["spiderman","superman","batman"];
+ let myObj={
+    name:"fahim"
+    age:12
+ }
